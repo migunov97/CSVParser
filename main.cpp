af3fbafefc3c3153319b630ff9c0db03225c2070
Victor <vmigunovv@mail.ru>
2022-12-27 10:32:40 +0300

    Firstly read all in the one string then parse using multithreading

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <string>
 #include <fstream>
+#include <thread>
 
 // Задача:
 // Нужно оптимизировать парсер csv любыми методами.
@@ -28,84 +30,138 @@ struct Row
     int passenger_count;
 };
 
-struct TableImpl
-{
-    std::vector<Row> rows;
-};
-
 class Table
 {
 public:
-    Table(std::vector<Row> &&rows) : rows(std::move(rows)) {}
+    Table(std::vector<std::vector<Row>> &&rows) : rows(std::move(rows)) {}
 
     uint64_t rows_count() const
     {
-        return rows.size();
+        uint64_t result = 0;
+        for (const auto &vector : rows)
+            result += vector.size();
+
+        return result;
     }
 
     Row get_row(uint64_t idx) const
     {
-        return rows[idx];
+        size_t i = 0;
+        for (;; ++i)
+        {
+            const size_t vectorSize = rows[i].size();
+            if (idx >= vectorSize)
+                idx -= vectorSize;
+            else
+                break;
+        }
+        return rows[i][idx];
     }
 
 private:
-    std::vector<Row> rows;
+    std::vector<std::vector<Row>> rows;
 };
 
-float stof1(const std::string &s) {
-    if(s.size() == 0) {
-        return 0.f;
+// Returns std::vector of rows, which are written in string from start to begin
+// size shows recommended size to reserve
+static std::vector<Row> getRows(std::string::iterator begin, std::string::iterator end,
+                         size_t size)
+{
+    std::vector<Row> rows;
+    rows.reserve(size);
+    auto it = begin;
+
+    while (it != end)
+    {
+
+        
+        Row row;
+
+        {
+            auto keyEnd = std::find(it, end, ',');
+            row.key.assign(&*it, std::distance(it, keyEnd));
+            it = ++keyEnd;
+        }
+
+        row.fare_amount = atof(&*it);
+        it = ++std::find(it, end, ',');
+
+        {
+            auto datetimeEnd = std::find(it, end, ',');
+            row.pickup_datetime.assign(&*it, std::distance(it, datetimeEnd));
+            it = ++datetimeEnd;
+        }
+
+        row.pickup_longitude = atof(&*it);
+        it = ++std::find(it, end, ',');
+
+        row.pickup_latitude = atof(&*it);
+        it = ++std::find(it, end, ',');
+
+        row.dropoff_longitude = atof(&*it);
+        it = ++std::find(it, end, ',');
+
+        row.dropoff_latitude = atof(&*it);
+        it = ++std::find(it, end, ',');
+
+        row.passenger_count = std::atoi(&*it);
+
+        it = ++std::find(it, end, '\n');
+
+        rows.push_back(row);
     }
-    return std::stof(s);
+    return rows;
 }
 
 Table read_csv(const char *filename)
 {
-    std::vector<Row> rows;
 
+    const size_t smallStringSize = 70;
+
+    // Read file like to a single string to make parallel parsing possible
     std::fstream file(filename);
-
-    if(!file.is_open()) {
+    if(!file.is_open())
         throw std::runtime_error("File does not exist");
-    }
 
+    file.seekg(0, std::ios::end);
+    size_t size = file.tellg();
+    std::string fileString(size, ' ');
+    file.seekg(0);
+    file.read(&fileString[0], size);
+
+    // skip header
+    auto it = ++std::find(fileString.begin(), fileString.end(), '\n');
+    size -= std::distance(fileString.begin(), it);
+
+    const auto threadsNb = std::thread::hardware_concurrency();
+    std::vector<std::vector<Row>> rows;
+    rows.resize(threadsNb);
+
+    std::vector<std::thread> threads;
+
+    const size_t substringSize = size / threadsNb;
+
+    for (size_t i = 0; i < threadsNb; ++i)
     {
-        // skip header
-        std::string key, fare_amount, pickup_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count;
-        std::getline(file, key, ',');
-        std::getline(file, fare_amount, ',');
-        std::getline(file, pickup_datetime, ',');
-        std::getline(file, pickup_longitude, ',');
-        std::getline(file, pickup_latitude, ',');
-        std::getline(file, dropoff_longitude, ',');
-        std::getline(file, dropoff_latitude, ',');
-        std::getline(file, passenger_count);
+        auto preliminaryBegin = fileString.begin() + i * substringSize;
+        auto begin = i == 0 ? it : ++std::find(preliminaryBegin, fileString.end(), '\n');
+        auto end = i == threadsNb -1  ?
+            fileString.end() :
+            ++std::find(fileString.begin() + (i + 1) * substringSize, fileString.end(), '\n'); 
+
+        // Parses part of string and form rows[i], 
+        // where i is a number of thread and number of fileString piece 
+        auto parsePart = [&rows, begin, end, smallStringSize, i](){       
+        rows[i] = getRows(begin, end, std::distance(begin, end) / smallStringSize);
+        };
+
+        threads.push_back(std::thread (parsePart));
     }
 
-    while (!file.eof())
+    for (auto& t : threads)
     {
-        Row row;
-        std::string key, fare_amount, pickup_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count;
-        std::getline(file, key, ',');
-        std::getline(file, fare_amount, ',');
-        std::getline(file, pickup_datetime, ',');
-        std::getline(file, pickup_longitude, ',');
-        std::getline(file, pickup_latitude, ',');
-        std::getline(file, dropoff_longitude, ',');
-        std::getline(file, dropoff_latitude, ',');
-        std::getline(file, passenger_count);
-
-        row.key = key;
-        row.fare_amount = stof1(fare_amount);
-        row.pickup_datetime = pickup_datetime;
-        row.pickup_longitude = stof1(pickup_longitude);
-        row.pickup_latitude = stof1(pickup_latitude);
-        row.dropoff_longitude = stof1(dropoff_longitude);
-        row.dropoff_latitude = stof1(dropoff_latitude);
-        row.passenger_count = std::stoi(passenger_count);
-
-        rows.push_back(row);
-    }
+          t.join();
+    }   
 
     return Table(std::move(rows));
 }
